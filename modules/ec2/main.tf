41968448c6db563c4b3e6ed3a94148f6613d9545
ashokreddy7777 <ashokvardhanreddy96@gmail.com>
2020-09-22 14:48:48 -0400

    added config files to deploy jenkins and sonarqube servers using terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,11 @@
+/*
 data "template_file" "test" {
          template = <<EOF
-#!/bin/bash
+!/bin/bash
 docker container run -d -it -p 80:8080 --name jenkins jenkins/jenkins:lts
          EOF
         }
+*/
 
 resource "aws_instance" "ec2" {
     ami                    = var.ami
@@ -11,7 +13,8 @@ resource "aws_instance" "ec2" {
     key_name               = var.key_name
     vpc_security_group_ids = var.vpc_security_group_ids
     subnet_id              = var.subnet_id
-    user_data              = base64encode(data.template_file.test.rendered)
+#    user_data              = base64encode(data.template_file.test.rendered)
+    user_data              = var.user_data
 
     tags                   = var.instance_tags
 }
